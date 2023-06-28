d83b2d5e0f12ec0b8c270752570cc6c4f69f7ca1
Cao-Van-Danh <convandanh1234@gmail.com>
2023-06-28 16:04:38 +0700

    Update: Thêm audio cho video

--- home.py
+++ home.py
@@ -1,150 +1,216 @@
 import os
+import tkinter as tk
+from tkinter import filedialog
 import random
 import subprocess
+import pysrt
+import shutil
+import ffmpeg
 
-# Đường dẫn đến các thư mục input và output
-video_input_folder = "VideoInput/"
-mp3_audio_folder = "Mp3Audio/"
-audio_voice_folder = "AudioVoice/"
-srt_voice_folder = "SrtVoice/"
-cache_folder = "Cache/"
-output_folder = "Output/"
 
-# Đọc danh sách các file Srt từ thư mục SrtVoice
-def read_srt_files():
-    srt_files = []
-    srt_voice_path = os.path.join(os.getcwd(), srt_voice_folder)
-    if os.path.exists(srt_voice_path):
-        for file_name in os.listdir(srt_voice_path):
-            if file_name.endswith(".srt"):
-                srt_files.append(os.path.join(srt_voice_path, file_name))
-    else:
-        print(f"Thư mục '{srt_voice_folder}' không tồn tại.")
-    return srt_files
+# FFMPEG processing functions
+def process_subtitles(srt_path):
+    subs = pysrt.open(srt_path, encoding="utf-8")
+    return [(sub.start.ordinal, sub.end.ordinal) for sub in subs]
 
-# Đọc danh sách timecode từ file Srt
-def extract_timecodes(srt_file):
-    timecodes = []
-    with open(srt_file, "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.strip()
-            if line.isdigit():
-                timecodes.append(line)
-    return timecodes
 
-# Chọn ngẫu nhiên một file video từ thư mục VideoInput
-def get_random_video():
-    video_files = [file for file in os.listdir(video_input_folder) if file.endswith((".mp4", ".avi", ".mkv"))]
-    random_video = random.choice(video_files)
-    return os.path.join(video_input_folder, random_video)
+def get_video_bitrate(video_file):
+    command = f"ffprobe -v error -select_streams v:0 -show_entries stream=bit_rate -of default=noprint_wrappers=1:nokey=1 {video_file}"
+    result = subprocess.check_output(command, shell=True)
+    bitrate = int(result)
+    return bitrate
 
-# Cắt video thành đoạn thời gian cụ thể
-def cut_video(input_file, output_file, start_time, end_time):
-    subprocess.call([
-        "ffmpeg",
-        "-i", input_file,
-        "-ss", start_time,
-        "-to", end_time,
-        "-c", "copy",
-        "-avoid_negative_ts", "1",
-        output_file
-    ])
 
-# Ghép video từ các đoạn video đã cắt
-def concat_videos(input_files, output_file):
-    with open("concat.txt", "w") as file:
-        for i, file_path in enumerate(input_files):
-            file.write(f"file '{file_path}'\n")
-        subprocess.call([
-            "ffmpeg",
-            "-f", "concat",
-            "-safe", "0",
-            "-i", "concat.txt",
-            "-c", "copy",
-            "-avoid_negative_ts", "1",
-            output_file
-        ])
-    os.remove("concat.txt")
+def process_videos(video_folder, timecodes, cache_folder):
+    output_files = []
+    last_video = None
 
-# Ghép audio voice vào video
-def merge_audio_video(video_file, audio_file, output_file):
-    subprocess.call([
-        "ffmpeg",
-        "-i", video_file,
-        "-i", audio_file,
-        "-c", "copy",
-        "-map", "0:v",
-        "-map", "1:a",
-        "-shortest",
-        output_file
-    ])
+    for idx, (start, end) in enumerate(timecodes):
+        videos = [
+            os.path.join(video_folder, file)
+            for file in os.listdir(video_folder)
+            if file.endswith(".mp4")
+        ]
 
-# Ghép nội dung của file Srt vào video
-def merge_subtitle_video(video_file, subtitle_file, output_file, position, font_size, font_color):
-    subprocess.call([
-        "ffmpeg",
-        "-i", video_file,
-        "-vf", f"subtitles='{subtitle_file}':force_style='Fontsize={font_size},PrimaryColour=&H{font_color[1:]}',drawtext=fontfile=arial.ttf:fontsize={font_size}:fontcolor={font_color}:x={position[0]}:y={position[1]}",
-        "-c:a", "copy",
-        output_file
-    ])
+        if last_video and last_video in videos:
+            videos.remove(
+                last_video
+            )  # Ensure the same video is not chosen consecutively
 
-# Ghép audio vào video
-def merge_audio_video(video_file, audio_file, output_file, audio_volume):
-    subprocess.call([
-        "ffmpeg",
-        "-i", video_file,
-        "-i", audio_file,
-        "-filter_complex", f"[0:a]volume={audio_volume}[v];[1:a]volume=1[a];[v][a]amix=inputs=2",
-        "-c:v", "copy",
-        "-map", "0:v",
-        "-map", "[a]",
-        output_file
-    ])
+        if not videos:
+            log_error("No video files found in the video folder", cache_folder)
+            break
 
-# Tạo thư mục nếu chưa tồn tại
-def create_directory(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+        video = random.choice(videos)  # Choose a random video from the list
+        last_video = video  # Store the last video chosen
 
-# Xóa các file trong thư mục cache
-def clear_cache():
-    for file_name in os.listdir(cache_folder):
-        file_path = os.path.join(cache_folder, file_name)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
+        # Calculate random start offset
+        start_offset = min(
+            random.randint(0, 300), (end - start)
+        )  # Random value between 0 and 3 seconds (in milliseconds), but not exceeding the timecode span
 
-# Bước 1: Đọc file Srt để lấy ra danh sách timecode từng dòng nội dung
-srt_files = read_srt_files()
-timecodes = []
-for srt_file in srt_files:
-    timecodes.extend(extract_timecodes(srt_file))
+        output_file = os.path.join(cache_folder, f"{idx:03}.mp4")
 
-# Bước 2: Cắt các video ngẫu nhiên từ VideoInput và lưu vào Cache
-clear_cache()
-for i, timecode in enumerate(timecodes):
-    video_file = get_random_video()
-    output_file = os.path.join(cache_folder, f"{str(i+1).zfill(3)}.mp4")
-    cut_video(video_file, output_file, "00:00:00", timecode)
+        # Cut the re-encoded video
+        command = f"ffmpeg -i {video} -ss {start_offset/1000} -to {(end - start + start_offset)/1000} -c:v libx264 -preset ultrafast -threads 4 -c:a aac {output_file}"
+        subprocess.run(command, shell=True)
 
-# Bước 3: Ghép các video đã cắt thành một video dài trong Cache
-concatenated_video = os.path.join(cache_folder, "VideoGhep.mp4")
-concat_videos(sorted(os.listdir(cache_folder)), concatenated_video)
+        output_files.append(output_file)
 
-# Bước 4: Ghép audio voice vào video đã ghép trong Cache
-audio_file = os.path.join(audio_voice_folder, os.path.splitext(os.path.basename(srt_files[0]))[0] + ".mp3")
-video_voice = os.path.join(cache_folder, "VideoVoice.mp4")
-merge_audio_video(concatenated_video, audio_file, video_voice)
+    return output_files
 
-# Bước 5: Ghép nội dung của file Srt vào video đã ghép trong Cache
-subtitle_file = srt_files[0]
-video_srt = os.path.join(cache_folder, "VideoSrt.mp4")
-merge_subtitle_video(video_voice, subtitle_file, video_srt, (100, 100), 24, "#FF0000")
 
-# Bước 6: Ghép audio vào video cuối cùng và lưu vào thư mục Output
-final_audio_file = os.path.join(mp3_audio_folder, random.choice(os.listdir(mp3_audio_folder)))
-final_video = os.path.join(output_folder, "Final.mp4")
-merge_audio_video(video_srt, final_audio_file, final_video, "0.5")
+def merge_videos(video_files, cache_folder, output_file):
+    video_list_file = os.path.join(cache_folder, "video_list.txt")
 
-print("Hoàn thành xử lý video!")
+    with open(video_list_file, "w") as f:
+        for video_file in video_files:
+            f.write(f"file '{video_file}'\n")
+
+    command = f"ffmpeg -f concat -safe 0 -i {video_list_file} -c copy {output_file}"
+    subprocess.run(command, shell=True)
+
+    return output_file
+
+
+def extract_audio(video_file, output_file):
+    stream = ffmpeg.input(video_file)
+    stream = ffmpeg.output(stream.audio, output_file)
+    ffmpeg.run(stream)
+
+
+def add_audio(video_file, audio_folder, cache_folder):
+    audio_files = [
+        os.path.join(audio_folder, file)
+        for file in os.listdir(audio_folder)
+        if file.endswith(".mp3")
+    ]
+    if not audio_files:
+        log_error("No audio files found in the audio folder", cache_folder)
+        return None
+
+    audio_file = random.choice(audio_files)
+    output_file = os.path.join(cache_folder, "video_with_audio.mp4")
+    command = f"ffmpeg -i {video_file} -i {audio_file} -c:v copy -c:a aac -strict experimental -map 0:v:0 -map 1:a:0 {output_file}"
+    subprocess.run(command, shell=True)
+
+    return output_file
+
+
+def add_subs(video_with_audio, srt_file, cache_folder):
+    output_file = os.path.join(cache_folder, "video_with_subs.mp4")
+    command = f"ffmpeg -i '{video_with_audio}' -vf subtitles='{srt_file}' -c:v libx264 -crf 23 -c:a aac -strict experimental '{output_file}'"
+    subprocess.run(command, shell=True)
+
+    return output_file
+
+
+def add_music(video_file, music_folder, cache_folder, output_file):
+    music_files = [
+        os.path.join(music_folder, file)
+        for file in os.listdir(music_folder)
+        if file.endswith(".mp3")
+    ]
+    if not music_files:
+        log_error("No music files found in the music folder", cache_folder)
+        return None
+
+    music_file = random.choice(music_files)
+    command = f"ffmpeg -i {video_file} -i {music_file} -filter_complex '[0:a][1:a]amix=inputs=2:duration=shortest' -c:v copy {output_file}"
+    subprocess.run(command, shell=True)
+
+    return output_file
+
+
+def log_error(error_message, cache_folder):
+    log_file = os.path.join(cache_folder, "Log.txt")
+    with open(log_file, "a") as f:
+        f.write(error_message + "\n")
+
+
+def select_folder(label):
+    folder = filedialog.askdirectory()
+    label.config(text=folder)
+
+
+def process(
+    video_folder, srt_folder, audio_folder, music_folder, cache_folder, output_folder
+):
+    timecodes = process_subtitles(os.path.join(srt_folder, "subtitles.srt"))
+    video_files = process_videos(video_folder, timecodes, cache_folder)
+    merged_video = merge_videos(
+        video_files, cache_folder, os.path.join(cache_folder, "merged.mp4")
+    )
+    video_with_audio = add_audio(merged_video, audio_folder, cache_folder)
+    if video_with_audio:
+        extracted_audio = os.path.join(cache_folder, "extracted_audio.aac")
+        extract_audio(video_with_audio, extracted_audio)
+        video_with_subs = add_subs(
+            video_with_audio, os.path.join(srt_folder, "subtitles.srt"), cache_folder
+        )
+        if video_with_subs:
+            final_video = os.path.join(output_folder, "final.mp4")
+            shutil.move(video_with_subs, final_video)
+            print(f"Final video saved to: {final_video}")
+
+
+root = tk.Tk()
+root.title("Video Processing Tool")
+
+video_label = tk.Label(root, text="Video folder")
+video_label.pack()
+video_button = tk.Button(
+    root, text="Select video folder", command=lambda: select_folder(video_label)
+)
+video_button.pack()
+
+srt_label = tk.Label(root, text="SRT folder")
+srt_label.pack()
+srt_button = tk.Button(
+    root, text="Select SRT folder", command=lambda: select_folder(srt_label)
+)
+srt_button.pack()
+
+audio_label = tk.Label(root, text="Audio folder")
+audio_label.pack()
+audio_button = tk.Button(
+    root, text="Select audio folder", command=lambda: select_folder(audio_label)
+)
+audio_button.pack()
+
+music_label = tk.Label(root, text="Music folder")
+music_label.pack()
+music_button = tk.Button(
+    root, text="Select music folder", command=lambda: select_folder(music_label)
+)
+music_button.pack()
+
+cache_label = tk.Label(root, text="Cache folder")
+cache_label.pack()
+cache_button = tk.Button(
+    root, text="Select cache folder", command=lambda: select_folder(cache_label)
+)
+cache_button.pack()
+
+output_label = tk.Label(root, text="Output folder")
+output_label.pack()
+output_button = tk.Button(
+    root, text="Select output folder", command=lambda: select_folder(output_label)
+)
+output_button.pack()
+
+process_button = tk.Button(
+    root,
+    text="Process",
+    command=lambda: process(
+        video_label.cget("text"),
+        srt_label.cget("text"),
+        audio_label.cget("text"),
+        music_label.cget("text"),
+        cache_label.cget("text"),
+        output_label.cget("text"),
+    ),
+)
+process_button.pack()
+
+root.mainloop()
